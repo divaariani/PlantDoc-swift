ecc89a8b4483a97cef1ebb1fc174a0ddc754bbbb
divaariani <divaarifinb@gmail.com>
2024-02-22 08:16:28 +0700

    Initial Commit

---
+++ plantdoc/plantdocApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantdocApp.swift
+//  plantdoc
+//
+//  Created by Diva Ariani on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct plantdocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
